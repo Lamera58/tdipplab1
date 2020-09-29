6321d138ac3fdc1cd5af3dbaf2111b6ef30101e2
Lamera58 <62799441+Lamera58@users.noreply.github.com>
2020-09-29 10:05:33 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main()
+{
+using namespace std;
+unsigned long int i,N;
+x float;
+cout <<  endl << "Enter N:";
+cin >> N;
+for (i=1;i<=N;i++) {x+=1/(float) i;};
+cout << endl << "The sum of series is " << x;
+system ("pause");
+ 
+}
